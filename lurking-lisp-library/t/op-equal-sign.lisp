c4cad0cd04c7c85a89841802501fee850ace3dfe
Shlomi Fish <shlomif@iglu.org.il>
2010-12-14 20:01:17 +0200

    Fix the quicklisp stuff.

--- lurking-lisp-library/t/op-equal-sign.lisp
+++ lurking-lisp-library/t/op-equal-sign.lisp
@@ -1,7 +1,16 @@
-(load "quicklisp.lisp")
+
+;;; The following lines added by ql:add-to-init-file:
+#-quicklisp
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+
 (ql:quickload "testbild")
 
+(use-package 'testbild)
 (load "lurking.lisp")
+
 (let ((producer (make-instance 'tap-producer)))
   (init-test producer)
   (emit-plan producer :plan-argument 1) ; simple plan, three tests planned
