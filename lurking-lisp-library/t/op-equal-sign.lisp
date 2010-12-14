dd9952a2bace09b895b7ee079a1c55e85125df30
Shlomi Fish <shlomif@iglu.org.il>
2010-12-14 20:27:35 +0200

    Add the concat-uc function to test.

--- lurking-lisp-library/t/op-equal-sign.lisp
+++ lurking-lisp-library/t/op-equal-sign.lisp
@@ -11,14 +11,23 @@
 (use-package 'testbild)
 (load "lurking.lisp")
 
+(defun concat-uc (x y)
+  (concatenate 'string x (string-upcase y)))
+
 (let ((producer (make-instance 'tap-producer)))
   (init-test producer)
-  (emit-plan producer :plan-argument 2) ; simple plan, three tests planned
+  (emit-plan producer :plan-argument 3) ; simple plan, three tests planned
   (let ((result 28))
     (-= result 4)
     ; TEST
     (emit-result producer :success (= result 24) :description "Test -=")
     (*= result 2)
     ; TEST
-    (emit-result producer :success (= result 48) :description "Test *="))
+    (emit-result producer :success (= result 48) :description "Test *=")
+
+    ; TEST
+    (emit-result producer :success (equal (concat-uc "foo " "bar") "foo BAR")
+                 :description "Testing concat-uc")
+
+    )
   (finalize-test producer))
