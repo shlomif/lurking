0680f86d54ffff7c486d4f1835f20d65d9eb60a9
Shlomi Fish <shlomif@iglu.org.il>
2010-12-14 20:17:52 +0200

    Convert -= and *= to the op2assign macro.
    This macro will be part of the interface.

--- lurking-lisp-library/lurking.lisp
+++ lurking-lisp-library/lurking.lisp
@@ -1,4 +1,13 @@
-(defmacro -= (x y)
-  `(incf ,x (- ,y)))
-(defmacro *= (x y)
-  `(setf ,x (* ,x ,y)))
+(defmacro op2assign (op)
+    (let ((op= (intern (concatenate 'string (string op) "="))))
+        `(defmacro ,op= (var . values)
+            `(setf ,var (,',op ,var ,@values)))))
+
+(op2assign -)
+(op2assign *)
+
+;;;(defmacro -= (x y)
+;;;  `(incf ,x (- ,y)))
+;;;(defmacro *= (x y)
+;;;  `(setf ,x (* ,x ,y)))
+
